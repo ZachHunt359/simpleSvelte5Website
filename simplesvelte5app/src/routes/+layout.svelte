6e4395d7029ad5609d81c1e25cb24096b47900ca
Zach Hunt <56412983+ZachHunt359@users.noreply.github.com>
2025-05-16 16:29:10 -0700

    Rough layout
    Very rough layout of the navigation bars

---
+++ simplesvelte5app/src/routes/+layout.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+    import '../styles/main.css';
+	let { children } = $props();
+</script>
+
+
+{@render children()}
--- simplesvelte5app/src/routes/+page.svelte
+++ simplesvelte5app/src/routes/+page.svelte
@@ -1,2 +1,23 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<nav class="top-nav">
+    <ul>
+        <li><a href="/#">Discord</a></li>
+        <li><a href="/#">Patreon</a></li>
+        <li><a href="/#">PARANOiD</a></li>
+        <li><a href="/#">Notifs</a></li>
+        <li><a href="/#">Inquiry</a></li>
+    </ul>
+</nav>
+
+<img 
+src="https://us-a.tapas.io/c/03/645560a6-d5fa-4fe7-ac66-fb7866104416.png?__token__=exp=1747437889~acl=/c/03/645560a6-d5fa-4fe7-ac66-fb7866104416.png*~hmac=4b98590656e326da588ad78aedb6469c7c1bbdcb1593e5588b39897e3dfe788d&version=v4" 
+alt="PARANOiD" />
+
+<nav class="bottom-nav">
+    <ul>
+        <li><a href="/#">Chapter Start</a></li>
+        <li><a href="/#">Back 1 Step</a></li>
+        <li><a href="/#">Chapter Select</a></li>
+        <li><a href="/#">Forward 1 Step</a></li>
+        <li><a href="/#">Save</a></li>
+    </ul>
+</nav>
