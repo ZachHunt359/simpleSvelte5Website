26687ba091a78988d99643f4757ccdb975dedb11
Zach Hunt <56412983+ZachHunt359@users.noreply.github.com>
2025-11-01 06:31:47 -0700

    Stop tracking generated build artifacts

--- build/server/chunks/constants.server-ClstHRc7.js
+++
@@ -1,9 +0,0 @@
-import { b as private_env } from './shared-server-DaWdgxVh.js';
-
-const one_day = 60 * 60 * 24;
-const AUTH_TOKEN_EXPIRY_SECONDS = Number(
-  private_env?.AUTH_TOKEN_EXPIRY_SECONDS ?? one_day * 365
-);
-
-export { AUTH_TOKEN_EXPIRY_SECONDS as A };
-//# sourceMappingURL=constants.server-ClstHRc7.js.map
